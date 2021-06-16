b9e3a92ca9233daea718a9a39a6057c0bbbf895a
LucioMart <luchoweb15@gmail.com>
2021-06-16 00:23:56 -0300

    Agregado metodos al controlador, para los endpoint de Post

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,8 +2,81 @@
 module Api 
     module V1 
         class PostsController < ApplicationController
+
+            def index
+                posts = Post.order('created_at').page(params[:page]).per(25);
+                render json: {
+                    status: 'EXITOSO',
+                    message: 'Posts cargados',
+                    data: posts
+                }, status: :ok
+            end  
             
-        end
+            def show
+                post = Post.find(params[:id])
+                render json: {
+                    status: 'EXITOSO',
+                    message: 'Post cargado',
+                    data: post    
+                }, status: :ok
+            end
+
+            def create
+                post = Post.new(character_params)
+                if post.save
+                    render json: {
+                        status: 'Exitoso',
+                        message: 'Post creado',
+                        data: post
+                    }, status: :ok
+                else 
+                    render json: {
+                        status: 'Exitoso',
+                        message: 'Post no Creado',
+                        data: post.errors
+                    }, status: :unprocessable_entity
+                end            
+            end
+
+            def destroy
+                post = Post.find(params[:id])
+                if post.destroy
+                    render json: {
+                        status: 'EXITOSO',
+                        message: 'Post eliminado',
+                        data: post
+                    }, status: :ok
+                else 
+                    render json: {
+                        status: 'FALLIDO',
+                        message: 'Post NO BORRADO',
+                        data: post.errors
+                    }, status: :unprocessable_entity
+                end
+            end
+
+            def update
+                post = Post.find(params[:id])
+                if post.update(character_params)
+                    render json: {
+                        status: 'EXITOSO',
+                        message: 'Post actualizado',
+                        data: post
+                    }, status: :ok
+                else 
+                    render json: {
+                        status: 'FALLIDO',
+                        message: 'Post NO ACTUALIZADO',
+                        data: post.errors
+                    }, status: :unprocessable_entity
+                end
+            end
+
+            private
+            def character_params
+                params.permit(:name, :year, :weigh, :story, :picture)
+            end    
+
+        end        
     end
 end
-
